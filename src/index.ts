4d02a74a20a07bdf13bb0df288b073a9d63fcf4c
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-11-25 00:45:33 +0900

    feat: generate .gitignore

--- src/index.ts
+++ src/index.ts
@@ -41,6 +41,7 @@ module.exports = {
       const hashDir = getHashDirPath(project);
       fs.mkdirSync(hashDir, { recursive: true });
       fs.writeFileSync(path.join(hashDir, 'hash'), hash);
+      fs.writeFileSync(path.join(hashDir, '.gitignore'), 'hash');
     }
 
     function getHashDirPath(project: Project): string {
