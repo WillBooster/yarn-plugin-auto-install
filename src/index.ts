b9756813f598e7d79282442e5a73519fb5d01820
Sakamoto, Kazunori <exkazuu@gmail.com>
2021-11-25 00:01:50 +0900

    feat: improve package.json finding

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,21 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-import type { Hooks } from '@yarnpkg/core';
+import type { Hooks, Project } from '@yarnpkg/core';
 
 module.exports = {
   name: 'plugin-auto-install',
   factory: () => {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const child_process = require('child_process');
     const crypto = require('crypto');
     const fs = require('fs');
     const path = require('path');
-    const { sync } = require('fast-glob');
 
-    function calcPackageHash(): string | void {
+    function calcPackageHash(project: Project): string | void {
       try {
-        const packageJsonText = fs.readFileSync('package.json', 'utf-8');
-        const packageJson = JSON.parse(packageJsonText);
-
-        let workspaces = packageJson.workspaces || [];
-        if (typeof workspaces === 'string') {
-          workspaces = [workspaces];
-        }
-        if (!Array.isArray(workspaces)) return;
-
-        const packageAndLockFiles = ['package.json', 'yarn.lock'];
-        for (const workspace of workspaces) {
-          packageAndLockFiles.push(...sync(path.join(workspace, 'package.json')));
+        const packageAndLockFiles: string[] = [];
+        for (const workspace of project.workspaces) {
+          packageAndLockFiles.push(path.join(workspace.cwd, 'package.json'));
         }
         packageAndLockFiles.sort();
 
@@ -32,50 +23,56 @@ module.exports = {
         for (const file of packageAndLockFiles) {
           const stat = fs.statSync(file, { throwIfNoEntry: false });
           if (!stat) continue;
-          hash.update(file);
           hash.update(stat.mtimeMs.toString());
         }
+        hash.update(project.lockFileChecksum);
         return hash.digest('hex');
       } catch (_) {
         // do nothing
       }
     }
 
-    function writePackageHash(hash: string): void {
-      const hashDir = path.join('.yarn', 'plugins', 'plugin-auto-install');
+    function readPackageHash(project: Project): string {
+      const hashDir = getHashDirPath(project);
+      return fs.readFileSync(path.join(hashDir, 'hash'), 'utf-8');
+    }
+
+    function writePackageHash(hash: string, project: Project): void {
+      const hashDir = getHashDirPath(project);
       fs.mkdirSync(hashDir, { recursive: true });
       fs.writeFileSync(path.join(hashDir, 'hash'), hash);
     }
 
-    return {
-      hooks: {
-        afterAllInstalled() {
+    function getHashDirPath(project: Project): string {
+      return path.join(project.cwd, '.yarn', 'plugins', 'plugin-auto-install');
+    }
+
+    const hooks: Hooks = {
+      afterAllInstalled(project) {
+        try {
+          const hash = calcPackageHash(project);
+          if (hash) writePackageHash(hash, project);
+        } catch (_) {
+          // do nothing
+        }
+      },
+      async wrapScriptExecution(executor, project): Promise<() => Promise<number>> {
+        try {
+          const hash = calcPackageHash(project);
           try {
-            const hash = calcPackageHash();
-            if (hash) writePackageHash(hash);
+            if (hash && hash === readPackageHash(project)) return executor;
           } catch (_) {
             // do nothing
           }
-        },
-        async wrapScriptExecution(executor): Promise<() => Promise<number>> {
-          try {
-            const hash = calcPackageHash();
-            try {
-              const hashDir = path.join('.yarn', 'plugins', 'plugin-auto-install');
-              const storedHash = fs.readFileSync(path.join(hashDir, 'hash'), 'utf-8');
-              if (storedHash && storedHash === hash) return executor;
-            } catch (_) {
-              // do nothing
-            }
-            console.info('plugin-auto-install detects changes in package.json and/or yarn.lock.');
-            if (hash) writePackageHash(hash);
-            child_process.spawnSync('yarn', ['install'], { env: process.env, stdio: 'inherit' });
-          } catch (_) {
-            // do nothing
-          }
-          return executor;
-        },
-      } as Hooks,
+          console.info('plugin-auto-install detects changes in package.json and/or yarn.lock.');
+          if (hash) writePackageHash(hash, project);
+          child_process.spawnSync('yarn', ['install'], { env: process.env, stdio: 'inherit' });
+        } catch (_) {
+          // do nothing
+        }
+        return executor;
+      },
     };
+    return { hooks };
   },
 };
