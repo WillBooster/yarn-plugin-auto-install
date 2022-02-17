24512d79bb97f3211eba60498501c4e605e2c505
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-02-17 10:31:55 +0900

    chore: wbfy this repo

---
+++ .lintstagedrc.cjs
@@ -0,0 +1,15 @@
+const micromatch = require('micromatch');
+
+module.exports = {
+  './{src,__tests__}/**/*.{cjs,js,jsx,mjs,ts,tsx}': ['eslint --fix', 'prettier --write'],
+  './**/*.{cjs,css,htm,html,js,json,json5,jsx,md,mjs,scss,ts,tsx,vue,yaml,yml}': (files) => {
+    files = micromatch.not(files, './{src,__tests__}/**/*.{cjs,js,jsx,mjs,ts,tsx}');
+    const filteredFiles = files.filter((file) => !file.includes('/test-fixtures/') && !file.includes('/packages/'));
+    if (filteredFiles.length === 0) return [];
+    const commands = [`prettier --write ${filteredFiles.join(' ')}`];
+    if (filteredFiles.some((file) => file.endsWith('package.json'))) {
+      commands.push('yarn sort-package-json');
+    }
+    return commands;
+  },
+};
