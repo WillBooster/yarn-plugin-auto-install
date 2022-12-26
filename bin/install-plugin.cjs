88567070df1de10ab3e3d71f85111a4ca0a8643e
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-12-26 22:50:50 +0900

    fix: .releaserc.json

--- bin/install-plugin.cjs
+++ bin/install-plugin.cjs
@@ -1,5 +1,5 @@
 const cp = require('child_process');
 const version = cp.execSync('npm show yarn-plugin-auto-install version').toString().trim();
-const command = `yarn plugin import https://github.com/WillBooster/yarn-plugin-auto-install/releases/download/v${version}/index.cjs`;
+const command = `yarn plugin import https://github.com/WillBooster/yarn-plugin-auto-install/releases/download/v${version}/index.min.cjs`;
 console.info(`$ ${command}`);
 cp.spawnSync(command, { shell: true, stdio: 'inherit' });
