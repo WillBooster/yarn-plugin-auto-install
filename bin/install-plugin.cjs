9e1778e9a256df395b1e5e10bc6dc5b521e6f450
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-03-08 10:11:13 +0900

    fix: 'yarn dlx yarn-plugin-auto-install'

--- bin/install-plugin.cjs
+++ bin/install-plugin.cjs
@@ -2,6 +2,6 @@
 
 const cp = require('child_process');
 const version = cp.execSync('npm show yarn-plugin-auto-install version').toString().trim();
-const command = `yarn plugin import https://github.com/WillBooster/yarn-plugin-auto-install/releases/download/v${version}/index.min.cjs`;
+const command = `yarn plugin import https://github.com/WillBooster/yarn-plugin-auto-install/releases/download/v${version}/index.cjs`;
 console.info(`$ ${command}`);
 cp.spawnSync(command, { shell: true, stdio: 'inherit' });
