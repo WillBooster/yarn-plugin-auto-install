c1981bcdc93bb9bde941275e27667c7812d605d4
Sakamoto, Kazunori <exkazuu@gmail.com>
2026-08-09 22:18:16 +0900

    chore: willboosterify this repo
    Applied @willbooster/wbfy v18.4.1.

--- .lefthook/post-merge/prepare.sh
+++ .lefthook/post-merge/prepare.sh
@@ -8,6 +8,6 @@ run_if_changed() {
   fi
 }
 
-run_if_changed "(mise\.toml|\.mise\.toml|\.tool-versions|\..+-version)" "mise install"
+run_if_changed "(mise\.toml|\.mise\.toml)" "mise install"
 if git diff --no-color -U0 ORIG_HEAD HEAD -- '*bunfig.toml' | grep --quiet -E '^[+-] *(globalStore|linker|publicHoistPattern)'; then rm -Rf -- 'node_modules'; fi
 run_if_changed "(package\.json|bun\.lock|bunfig\.toml|\.npmrc|patches/)" "bun install"
